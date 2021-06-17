9bd91261d8742365f09151812de612c5682f20a8
gamon <blazetime4@gmail.com>
2021-06-17 16:12:11 +0900

    업로드오류처리끝

--- client/src/components/views/UploadProductPage/UploadProductPage.js
+++ client/src/components/views/UploadProductPage/UploadProductPage.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useState } from 'react';
 import { Typography, Button, Form, Input } from 'antd';
 import FileUpload from '../../utils/FileUpload';
 import Axios from 'axios';
@@ -6,74 +6,72 @@ import Axios from 'axios';
 const { TextArea } = Input;
 
 const Continents = [
-    { key: 1, value: "냉동식품" },
-    { key: 2, value: "신선식품" },
-    { key: 3, value: "기타식품" },
-]
+  { key: 1, value: '냉동식품' },
+  { key: 2, value: '신선식품' },
+  { key: 3, value: '기타식품' },
+];
 
 function UploadProductPage() {
+  const [Title, setTitle] = useState('');
+  const [Description, setDescription] = useState('');
+  const [Price, setPrice] = useState(0);
+  const [Continent, setContinent] = useState(1);
+  const [Images, setImages] = useState([]);
 
-    const [Title, setTitle] = useState("")
-    const [Description, setDescription] = useState("")
-    const [Price, setPrice] = useState(0)
-    const [Continent, setContinent] = useState(1)
-    const [Images, setImages] = useState([])
+  const titleChangeHandler = (event) => {
+    setTitle(event.currentTarget.value);
+  };
 
-    const titleChangeHandler = (event) => {
-        setTitle(event.currentTarget.value)
-    }
+  const descriptionChangeHandler = (event) => {
+    setDescription(event.currentTarget.value);
+  };
 
-    const descriptionChangeHandler = (event) => {
-        setDescription(event.currentTarget.value)
-    }
+  const priceChangeHandler = (event) => {
+    setPrice(event.currentTarget.value);
+  };
 
-    const priceChangeHandler = (event) => {
-        setPrice(event.currentTarget.value)
-    }
+  const continentChangeHandler = (event) => {
+    setContinent(event.currentTarget.value);
+  };
 
-    const continentChangeHandler = (event) => {
-        setContinent(event.currentTarget.value)
-    }
+  return (
+    <div style={{ maxWidth: '700px', margin: '2rem auto' }}>
+      <div style={{ textAlign: 'center', marginBottom: '2rem' }}>
+        <h2>상품 업로드</h2>
+      </div>
 
-    return (
-        <div style={{ maxWidth: '700px', margin: '2rem auto' }}>
-            <div style={{ textAlign: 'center', marginBottom: '2rem' }}>
-                <h2>상품 업로드</h2>
-            </div>
+      <Form>
+        {/* DropZone */}
+        <FileUpload />
 
-            <Form>
-                {/* DropZone */}
-                <FileUpload />
-
-                <br />
-                <br />
-                <label>이름</label>
-                <Input onChange={titleChangeHandler} value={Title} />
-                <br />
-                <br />
-                <label>설명</label>
-                <TextArea onChange={descriptionChangeHandler} value={Description} />
-                <br />
-                <br />
-                <label>가격(원)</label>
-                <Input type="number" onChange={priceChangeHandler} value={Price} />
-                <br />
-                <br />
-                <select onChange={continentChangeHandler} value={Continent}>
-                    {Continents.map(item => (
-                        <option key={item.key} value={item.key}> {item.value}</option>
-                    ))}
-                </select>
-                <br />
-                <br />
-                <button type="submit">
-                    확인
-                </button>
-            </Form>
-
-
-        </div>
-    )
+        <br />
+        <br />
+        <label>이름</label>
+        <Input onChange={titleChangeHandler} value={Title} />
+        <br />
+        <br />
+        <label>설명</label>
+        <TextArea onChange={descriptionChangeHandler} value={Description} />
+        <br />
+        <br />
+        <label>가격(원)</label>
+        <Input type="number" onChange={priceChangeHandler} value={Price} />
+        <br />
+        <br />
+        <select onChange={continentChangeHandler} value={Continent}>
+          {Continents.map((item) => (
+            <option key={item.key} value={item.key}>
+              {' '}
+              {item.value}
+            </option>
+          ))}
+        </select>
+        <br />
+        <br />
+        <button type="submit">확인</button>
+      </Form>
+    </div>
+  );
 }
 
-export default UploadProductPage
+export default UploadProductPage;
